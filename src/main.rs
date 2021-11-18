1572e359b2ee047a714f6555d97ea1dfa0c16ca5
ktama <ktama@users.noreply.github.com>
2021-11-18 21:27:35 +0900

    add: read config

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,30 @@ mod tcp_server;
 
 fn main() -> io::Result<()> {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+    debug!("initilized logger");
 
-    error!("error log");
-    warn!("warn log");
-    info!("info log");
-    debug!("debug log");
+    let is_server = CONFIG["load_test"]["is_server"].as_bool().unwrap();
+    let protocol = CONFIG["load_test"]["protocol"].as_str().unwrap();
+    let mode = (if is_server { "server" } else { "client" }, protocol);
+
+    info!("Load Test Mode: {} & {}", mode.0, mode.1);
+    match mode {
+        ("client", "tcp") => {
+            info!("Tcp Client");
+            let target = CONFIG["load_test"]["target"].as_str().unwrap();
+        }
+        ("client", "udp") => {
+            info!("Udp Client");
+        }
+        ("server", "tcp") => {
+            info!("Tcp Server");
+        }
+        ("server", "udp") => {
+            info!("Udp Server");
+        }
+        _ => error!("Errors in the configuration file"),
+    }
+
     let bind_config_str = CONFIG["load_test"]["target"].as_str().unwrap();
     let bind_config = bind_config_str.parse().unwrap();
     let size_config_integer = CONFIG["load_test"]["packet_size"].as_integer().unwrap();
